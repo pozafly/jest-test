24097ba4af6f5068b1874d89d8fa58869c90654c
pozafly <pozafly@gmail.com>
2021-07-08 11:38:05 +0900

    asynchronus test

---
+++ test/Asynchronus/Callback.test.js
@@ -0,0 +1,60 @@
+/**
+ * NOTE: 콜백 함수 테스트
+ * JS 특성 상 비동기로 돌아가는 코드 작성 많음.
+ * Jest Runner가 비동기 코드 테스트라는 사실을 인지할 수 있도록
+ * 테스트 코드를 작성해주어야 함.
+ */
+
+/**
+ *
+ * @param {string} id
+ * @param {function} cb
+ */
+function fetchUser(id, cb) {
+  setTimeout(() => {
+    console.log('wait 0.1 sec.');
+    const user = {
+      id,
+      name: `User${id}`,
+      email: `${id}@test.com`,
+    };
+    cb(user);
+  }, 100);
+}
+
+/**
+ * wait 0.1 sec. 라는 콘솔이 찍히고,
+ * Test는 통과하지만,
+ * Cannot log after tests are done. Did you forget to wait for something async in your test?
+ * 위와 같은 문구로 비동기 처리하는 것에 대한 이야기를 해준다.
+ */
+test('fetch a user', () => {
+  fetchUser(1, (user) => {
+    expect(user).toEqual({
+      id: 1,
+      name: 'User1',
+      email: '1@test.com',
+    });
+  });
+});
+
+/**
+ * Jest에게 명시적으로 이 테스트 함수는 비동기 코드 테스트 하니,
+ * 콜백 함수가 호출되는지도 봐달라고 알려주면 된다.
+ *
+ * done을 인자에 넣고,
+ * 마지막에 호출해주는 과정이 Jest에게 비동기 임을 알려주는 것.
+ *
+ * 만약 아래의 코드가 실패하는 코드라면, Test 시간이 대폭 늘어난다.
+ * fetUser(2, (...)) 이렇게하면..
+ */
+test('fetch a user', (done) => {
+  fetchUser(1, (user) => {
+    expect(user).toEqual({
+      id: 1,
+      name: 'User1',
+      email: '1@test.com',
+    });
+    done();
+  });
+});
---
+++ test/Asynchronus/Promise.test.js
@@ -0,0 +1,39 @@
+// NOTE: 프로미스 테스트
+
+/**
+ *
+ * @param {string} id
+ * @returns {Promise}
+ */
+function fetchUser(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('wait 0.1 sec.');
+      const user = {
+        id,
+        name: `User${id}`,
+        email: `${id}@test.com`,
+      };
+      resolve(user);
+    }, 100);
+  });
+}
+
+/**
+ * then을 사용해 Promise를 테스트 함.
+ * 해결 방법은, return 문만 추가해주면 된다.
+ * 테스트 함수가 Promise를 return 하면 Jest가 resolve될 때까지 기다려줌.
+ */
+// test('fetch a user', () => {
+//   return fetchUser(1).then((user) => {
+//     (...)
+//   });
+// });
+test('fetch a user', () =>
+  fetchUser(1).then((user) => {
+    expect(user).toEqual({
+      id: 1,
+      name: 'User1',
+      email: '1@test.com',
+    });
+  }));
